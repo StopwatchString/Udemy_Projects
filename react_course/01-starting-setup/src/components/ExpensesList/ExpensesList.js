af25fa56981f5bfdfff178047b2b23ee8b98286a
Mason Speck <mcs.5299@gmail.com>
2023-05-26 20:47:05 -0400

    Pulled expenses content logic from JSX

--- react_course/01-starting-setup/src/components/ExpensesList/ExpensesList.js
+++ react_course/01-starting-setup/src/components/ExpensesList/ExpensesList.js
@@ -15,23 +15,26 @@ const ExpensesList = (props) => {
     return expense.date.getFullYear() === filterYear;
   });
 
+  let expensesContent = <p>No expenses found.</p>;
+
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
+  }
+
   return (
     <Card className="expenses">
       <ExpensesFilter
         filterYear={filterYear}
         onFilterSet={onFilterSetHandler}
       />
-
-      {filteredExpenses.map((expense) => {
-        return (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        );
-      })}
+      {expensesContent}
     </Card>
   );
 };
