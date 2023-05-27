a0ff491742802ea7f85ea250eced0a9f4cee106b
Mason Speck <mcs.5299@gmail.com>
2023-05-27 11:29:24 -0400

    Added expenses chart

--- react_course/01-starting-setup/src/components/Expenses/Expenses.js
+++ react_course/01-starting-setup/src/components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import "./Expenses.css";
 import Card from "../UI/Card";
 import ExpensesFilter from "../ExpensesFilter/ExpensesFilter";
 import ExpensesList from "../ExpensesList/ExpensesList";
+import ExpensesChart from "../ExpensesChart/ExpensesChart";
 
 const Expenses = (props) => {
   const [filterYear, setFilterYear] = useState(2021);
@@ -21,6 +22,7 @@ const Expenses = (props) => {
         filterYear={filterYear}
         onFilterSet={onFilterSetHandler}
       />
+      <ExpensesChart expenses={filteredExpenses} />
       <ExpensesList expenses={filteredExpenses} />
     </Card>
   );
