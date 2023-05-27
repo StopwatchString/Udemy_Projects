dfa8a2d136c5c5df275ce7686977ebea025b8928
Mason Speck <mcs.5299@gmail.com>
2023-05-26 21:36:30 -0400

    Added buttons to enable and disable form for adding expenses

--- react_course/01-starting-setup/src/components/NewExpense/NewExpense.js
+++ react_course/01-starting-setup/src/components/NewExpense/NewExpense.js
@@ -1,20 +1,40 @@
 import "./NewExpense.css";
 import ExpenseForm from "../ExpenseForm/ExpenseForm";
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
+import { useState } from "react";
 const NewExpense = (props) => {
+  const [formActive, setFormActive] = useState(false);
 
   const onSaveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: uuidv4(),
-    }
-  
+    };
+
     props.onAddExpense(expenseData);
-  }
+  };
+
+  const activateForm = () => {
+    setFormActive(true);
+  };
+
+  const deactivateForm = () => {
+    setFormActive(false);
+  };
 
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={onSaveExpenseDataHandler} />
+      {!formActive && (
+        <button onClick={activateForm} className="new-expense__actions">
+          Add New Expense
+        </button>
+      )}
+      {formActive && (
+        <ExpenseForm
+          onSaveExpenseData={onSaveExpenseDataHandler}
+          onCancelForm={deactivateForm}
+        />
+      )}
     </div>
   );
 };
